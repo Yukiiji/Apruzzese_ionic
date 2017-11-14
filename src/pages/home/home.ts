830145667df07b5c3774dcd14ce15734a8418ef5
Yukiiji <raph.yukiji@gmail.com>
2017-11-14 09:05:09 +0100

    Maj

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  app: AppData;
+  clickmessage: String;
 
+  Clickme(){
+    this.clickmessage="Bienvenue";
+  }
   constructor(public navCtrl: NavController) {
+    this.app=new AppData("nom app", 0.3);
+  }
+}
+
+class AppData{
+  constructor(public nom:string, public version: Number){
 
   }
-
 }
